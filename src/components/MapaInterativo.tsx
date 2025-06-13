e300c0164d988663b6d414a9ab8dc4d61f751895
JuanMarks <juandev77@gmail.com>
2025-06-12 21:23:37 -0300

    css: alterações no css

--- src/components/MapaInterativo.tsx
+++ src/components/MapaInterativo.tsx
@@ -79,9 +79,9 @@ const MapaInterativo = () => {
                 style={{ height: '100vh', width: '100%'}}
                 className='map-container'
             >
-                <TileLayer
-                    attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-                    url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+               <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
 
                 <MapClickHandler />
