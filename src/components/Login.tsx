ad7af48f6177f929845a918c0aa6cc7c1713deca
Agostinho Alves <aalvesdeoliveirafilho1@gmail.com>
2025-07-03 19:41:11 -0300

    fix: alterações

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -97,7 +97,7 @@ export default function Login() {
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                                 placeholder="Digite seu email"
-                                className="mt-1 block w-full bg-white text-white placeholder-blue-500 border-none rounded-md px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-900"
+                                className="mt-1 block w-full bg-white text-blue-500 placeholder-blue-500 border-none rounded-md px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-900"
                             />
                         </div>
 
@@ -113,7 +113,7 @@ export default function Login() {
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                                 placeholder="Digite sua senha"
-                                className="mt-1 block w-full bg-white text-white placeholder-blue-500 border-none rounded-md px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-900"
+                                className="mt-1 block w-full bg-white text-blue-500 placeholder-blue-500 border-none rounded-md px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-900"
                             />
                         </div>
                         
